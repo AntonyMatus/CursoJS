eeb6a2518a59397800f69934007aaf09b3217072
tony_ <antonyrebolledo@gmail.com>
2023-01-17 13:12:51 -0600

    tarea 1

---
+++ modulo 1/index.js
@@ -0,0 +1,15 @@
+
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+/*
+    Esta es la parte intermedia de nuestro proyecto
+    En esta parte realizamos los chequeos necesarios
+    Por ultimo vamos a la ultima linea de saludo
+    Esto tambien esta comentando
+    Aqui también
+*/
+
+
+// Este va a ser el mensaje que se envia para despedir
+console.log("Adiós");
